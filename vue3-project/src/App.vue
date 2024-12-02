47396bb21a70bebb0dafce8f96ac3d466485b21e
gydud25416 <gydud25416@gmail.com>
2024-12-03 00:01:29 +0900

    [Sec:13] v-show, v-if

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,16 +1,21 @@
-<template>
-  <h2>To-Do List</h2>
+<template> 
   <div class="container">
-    <form class="d-flex" @submit.prevent="onSubmit">
-      <div class="flex-grow-1  mr-2">
-        <input class="form-control" type="text" v-model="todo" placeholder="Type new to-do" >
+  <h2>To-Do List</h2>
+    <form @submit.prevent="onSubmit">
+      <div class="d-flex">
+        <div class="flex-grow-1  mr-2">
+          <input class="form-control" type="text" v-model="todo" placeholder="Type new to-do" >
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit" >
+            Add
+          </button>
+        </div>
       </div>
-      <div>
-        <button class="btn btn-primary" type="submit" >
-          Add
-        </button>
-      </div>
-    </form>
+        <div v-show="hasError" style="color:red">
+          This field cannot be empty
+        </div>
+      </form>
     <div v-for="todo in todos" :key="todo.id" class="card mt-2">
       <div class="card-body p-2">
         {{ todo.subject }}
@@ -28,19 +33,27 @@ export default {
       {id:1, subject:'휴대폰 사기'},
       {id:2, subject:'장보기'},
     ]);
+    const hasError = ref(false);
 
     const onSubmit = () => {
+      if(todo.value === ''){
+        hasError.value = true;
+        return;
+      }else{
       todos.value.push({
         id: Date.now(),
         subject: todo.value
       });
+      hasError.value = false;
+    }
     } 
+ 
     return({
       todo,
       onSubmit,
-      todos
+      todos,
+      hasError,
     })
-
   }
 }
 </script> 
