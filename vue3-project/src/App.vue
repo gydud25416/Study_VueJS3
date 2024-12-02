3f50b66521ff2bc9006d0b52e0f25adce695aaab
gydud25416 <gydud25416@gmail.com>
2024-12-02 23:29:26 +0900

    [Sec1:09~11] 양방향 바인딩 v-model

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,27 +1,49 @@
 <template>
-  <div :class="nameClass">{{name}}</div>
-  <input :type="type" :value="name">
-  <button @click="updateName">Click</button>
+  <h2>To-Do List</h2>
+  <div class="container">
+    <form class="d-flex" @submit.prevent="onSubmit">
+      <div class="flex-grow-1  mr-2">
+        <input class="form-control" type="text" v-model="todo" placeholder="Type new to-do" >
+      </div>
+      <div>
+        <button class="btn btn-primary" type="submit" >
+          Add
+        </button>
+      </div>
+    </form>
+    <div class="card mt-2">
+      <div class="card-body p-2">
+        {{ todos[0].subject }}
+      </div>
+    </div>
+    <div class="card mt-2">
+      <div class="card-body p-2">
+        {{ todos[1].subject }}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { ref } from 'vue';
 export default {
   setup(){
-    const name = ref('Kossie');
-    const type = ref('number');
-    const nameClass = ref('');
+    const todo = ref('');
+    const todos = ref([
+      {id:1, subject:'휴대폰 사기'},
+      {id:2, subject:'장보기'},
+    ]);
 
-    const updateName = () => {
-      name.value = 'Coder';
-      type.value = 'text';
-      nameClass.value = 'name'
-    }
+    const onSubmit = () => {
+      todos.value.push({
+        id: Date.now(),
+        subject: todo.value
+      });
+    } 
     return({
-      name,
-      updateName,
-      type,
-      nameClass,
+      todo,
+      onSubmit,
+      todos
     })
 
   }
